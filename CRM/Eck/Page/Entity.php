af646e7eb331aeaf5473a512a058ecf67615a018
Jens Schuppe <schuppe@systopia.de>
2021-10-28 14:56:09 +0200

    (wip) Entity view

--- CRM/Eck/Page/Entity.php
+++ CRM/Eck/Page/Entity.php
@@ -18,6 +18,37 @@ use CRM_Eck_ExtensionUtil as E;
 class CRM_Eck_Page_Entity extends CRM_Core_Page {
 
   public function run() {
+
+    // TODO: Get ECK entity type from request query.
+    if (!$entity_type_name = CRM_Utils_Request::retrieve('type', 'String', $this)) {
+      throw new CRM_Core_Exception('No entity type given.');
+    }
+    try {
+      $entity_type = civicrm_api3('EckEntityType', 'getsingle', ['name' => $entity_type_name]);
+      $this->assign('entity_type', $entity_type);
+    }
+    catch (Exception $exception) {
+      throw new Exception(E::ts('Invalid entity type.'));
+    }
+
+    // TODO: Get ECK entity ID from request query.
+    if (!$entity_id = CRM_Utils_Request::retrieve('id', 'Integer', $this)) {
+      throw new CRM_Core_Exception('No entity ID given.');
+    }
+
+    // TODO: Get ECK entity using the API.
+    $entity = civicrm_api3('Eck' . $entity_type['name'], 'getsingle', ['id' => $entity_id]);
+
+    // TODO: Get fields for subtype of the entity.
+    $fields = civicrm_api3('Eck' . $entity_type['name'], 'getfields', ['subtype' => $entity['subtype']]);
+    $this->assign('fields', $fields);
+
+    // TODO: Set page title.
+    CRM_Utils_System::setTitle($entity['title']);
+
+    // TODO: Assign entity properties as template variables.
+    $this->assign('entity', $entity);
+
     parent::run();
   }
 
