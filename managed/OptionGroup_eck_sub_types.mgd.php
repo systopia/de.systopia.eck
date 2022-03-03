bcb29f2f319b3edf781f12321c13491eb4e9de37
Coleman Watts <coleman@civicrm.org>
2022-03-03 15:04:28 -0500

    Switch eck_sub_types OptionGroup to a managed entity

---
+++ managed/OptionGroup_eck_sub_types.mgd.php
@@ -0,0 +1,21 @@
+<?php
+return [
+  [
+    'name' => 'OptionGroup_eck_sub_types',
+    'entity' => 'OptionGroup',
+    'cleanup' => 'always',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'eck_sub_types',
+        'title' => 'ECK Subtypes',
+        'description' => 'Entity Construction Kit Entity Subtypes',
+        'data_type' => NULL,
+        'is_reserved' => TRUE,
+        'is_active' => TRUE,
+        'is_locked' => TRUE,
+      ],
+    ],
+  ],
+];
