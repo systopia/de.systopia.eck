362318e19503d1fc0ccf08fde2a35b1196c83c4b
colemanw <coleman@civicrm.org>
2023-06-21 17:21:18 -0700

    Change maxlength of name to match mysql table name limit

--- CRM/Eck/Upgrader.php
+++ CRM/Eck/Upgrader.php
@@ -107,4 +107,15 @@ class CRM_Eck_Upgrader extends CRM_Extension_Upgrader_Base {
     return TRUE;
   }
 
+  /**
+   * Implements hook_civicrm_upgrade_N().
+   */
+  public function upgrade_0014() {
+    $this->ctx->log->info('Update name column in civicrm_eck_entity_type');
+    CRM_Core_DAO::executeQuery("ALTER TABLE `civicrm_eck_entity_type`
+      ALTER COLUMN `name` varchar(58) NOT NULL COMMENT 'The entity type name, also used in the sql table name'");
+
+    return TRUE;
+  }
+
 }
