2371469e9e4f77b559f16e3b84bac49e5cef5129
Jens Schuppe <schuppe@systopia.de>
2024-02-07 15:41:29 +0100

    PHPStan fixes

--- CRM/Eck/BAO/Entity.php
+++ CRM/Eck/BAO/Entity.php
@@ -21,7 +21,7 @@ use Civi\Eck\Permissions;
 
 class CRM_Eck_BAO_Entity extends CRM_Eck_DAO_Entity implements HookInterface {
 
-  public static function getEntityType($entityName) {
+  public static function getEntityType($entityName): ?string {
     return strpos($entityName, 'Eck_') === 0 ? substr($entityName, strlen('Eck_')) : NULL;
   }
 
@@ -30,7 +30,7 @@ class CRM_Eck_BAO_Entity extends CRM_Eck_DAO_Entity implements HookInterface {
    * @param int $entityId
    * @return string
    */
-  public static function getEntityIcon(string $entityName, int $entityId):?string {
+  public static function getEntityIcon(string $entityName, int $entityId): ?string {
     $default = self::$_icon;
     foreach (\CRM_Eck_BAO_EckEntityType::getEntityTypes() as $entity_type) {
       if ($entity_type['entity_name'] === $entityName) {
