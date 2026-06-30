e9f4dcba000985e6e3a06b7c1a0a532110ba7fcf
Jens Schuppe <schuppe@systopia.de>
2026-06-30 15:33:05 +0200

    Set version to 1.1.0-beta5

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.eck',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c104180bfe7327d0838a18598e0732423ad62ed3',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.1.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.eck' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c104180bfe7327d0838a18598e0732423ad62ed3',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.1.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
